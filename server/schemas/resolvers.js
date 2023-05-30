da148769241e77daf50719793cb58fcacc3f91db
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-05-30 18:31:29 -0400

    worked on resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -27,13 +27,30 @@ const resolvers = {
         upload: async (parent, {_id}) => {
             return await Upload.findById(_id).populate('genre');
         },
-        user: async(parent, args, context) => {
+        order: async (parent, {_id}, context) => {
             if(context.user) {
                 const user = await User.findById(context.user._id).populate({
                     path: 'orders.uploads',
                     populate: 'genre'
                 });
-                return user.orders.id(_id);
+                return await user.orders.id(_id)
+            }
+            throw new AuthenticationError('Oops! You need to log in!')
+        },
+        user: async(parent, args, context) => {
+            if(context.user) {
+                const user = await User.findById(context.user._id).populate(
+                    {
+                    path: 'wishlist.uploads',
+                    populate: 'genre'
+                    },
+                    {
+                        path: 'uploads',
+                        populate: 'genre'
+                    }
+                    //put order here? or keep under order
+                );
+                return user.wishlist.id(_id) && user.uploads.id(_id);
             }
             throw new AuthenticationError('Oops! You need to log in!');
         },
@@ -68,8 +85,76 @@ const resolvers = {
                 payment_method_types: ['card'],
                 line_items,
                 mode: 'payment',
-                success_url: `${url}`
-            })
+                success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+                cancel_url: `${url}/`
+            });
+            return {session: session.id}
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return {token, user};
+        },
+        updateUser: async (parent, args, context) => {
+            if(context.user) {
+                return await User.findByIdAndUpdate(context.user._id, args, {new: true});
+            }
+            throw new AuthenticationError('Oops! You need to log in!')
+        },
+        addOrder: async (parent, {uploads}, context) => {
+            console.log(context);
+            if(context.user) {
+                const order = new Order({uploads});
+                await User.findByIdAndUpdate(context.user._id, {$push: {uploads: upload }});
+
+                return order;
+            }
+            throw new AuthenticationError('Oops! You need to log in!');
+        },
+        deleteOrder: async (parent, {orderId}, context) => {
+            console.log(context);
+            if(context.user) {
+                const order = await Order.findOneAndDelete({
+                    _id: orderId,
+                });
+                await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$pull: {orders: order._id}}
+                );
+                return order;
+            }
+            throw new AuthenticationError('Oops! You need to log in!')
+        },
+        addUpload: async (parent, args, context) => {
+            if(context.user) {
+                const upload = await Upload.create(args);
+                await User.findOneAndUpdate( 
+                    {_id: context.user._id},
+                    {$push: {uploads: upload._id}}
+                );
+
+                return upload;
+            };
+            throw new AuthenticationError('Oops! You need to log in!')
+        },
+        updateUpload: async (parent, args, context) => {
+            if(context.user) {
+                const upload = await Upload
+            }
+            
+        },
+        deleteUpload: async () => {
+
+        },
+        addWishList: async() => {
+
+        },
+        deleteWishlist: async() => {
+
         }
     }
 }
