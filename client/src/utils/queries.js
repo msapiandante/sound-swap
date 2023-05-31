9468be5282558864a7201edcda500f7c34a42812
Melanie S <melanie.sapiandante@gmail.com>
2023-05-31 10:44:12 -0400

    added queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,70 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($genre: ID) {
+    products(genre: $genre) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      genre {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      quantity
+      genre {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_GENRE = gql`
+  {
+    genre {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
+      }
+    }
+  }
+`;
